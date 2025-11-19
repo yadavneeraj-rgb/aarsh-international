1a773b7f5aba6066ef503f9bc52fc67a373beb98
yadavneeraj-rgb <yadav.neeraj@pearlorganisation.com>
2025-11-19 17:05:19 +0530

    git ignore file

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Login | Neeraj - Ecommerce</title>
+    <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
