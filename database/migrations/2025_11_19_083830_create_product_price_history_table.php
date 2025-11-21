d417a493c34cc0e8635e38515d104df8869d1e1e
arunpearl12390 <arunkumar@pearlorganisation.com>
2025-11-21 09:28:59 +0530

    Initial commit on new branch

--- database/migrations/2025_11_19_083830_create_product_price_history_table.php
+++ database/migrations/2025_11_19_083830_create_product_price_history_table.php
@@ -8,18 +8,21 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('product_price_history', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('product_id')->constrained('product')->onDelete('cascade');
-            $table->decimal('previous_price', 10, 2)->default(0);
-            $table->decimal('new_price', 10, 2);
-            $table->decimal('price_change', 10, 2);
-            $table->decimal('percentage_change', 8, 2);
-            $table->date('change_date');
-            $table->timestamps();
-            
-            $table->index(['product_id', 'change_date']);
-        });
+       if (!Schema::hasTable('product_price_history')) {
+    Schema::create('product_price_history', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('product_id')->constrained('product')->onDelete('cascade');
+        $table->decimal('previous_price', 10, 2)->default(0);
+        $table->decimal('new_price', 10, 2);
+        $table->decimal('price_change', 10, 2);
+        $table->decimal('percentage_change', 8, 2);
+        $table->date('change_date');
+        $table->timestamps();
+
+        $table->index(['product_id', 'change_date']);
+    });
+}
+
     }
 
     public function down()
